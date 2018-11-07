64a76599c3f54e6e547a9aba1e28e85609e3ccb2
skayth1997 <hayk.petrosyan0597@gmail.com>
2018-11-07 16:20:27 +0400

    change

---
+++ Flickr/script.js
@@ -0,0 +1,17 @@
+$('#form').submit(function () {
+
+    var data = $('#form').serialize();
+
+    $.ajax({
+        type: 'GET',
+        url: 'flickr.php',
+        dataType: 'json',
+        data: data,
+        success: function (data) {
+            // console.log(data);
+            console.log(data[0]);
+        }
+    });
+    return false;
+
+});
